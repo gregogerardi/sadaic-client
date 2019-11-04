3cc16df96e94b5f97aef94c52e8c3e214d511234
Gregorio Gerardi <Gregorio.Gerardi@ibm.com>
2019-11-03 22:33:23 -0300

    Updates

---
+++ precache-manifest.c8665e5fb06ea1f7ef90e5818a2646b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30ac7737f8660d7853873686c172233",
+    "url": "/sadaic-client/index.html"
+  },
+  {
+    "revision": "030fd1946d3513991185",
+    "url": "/sadaic-client/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "da0d413f7fb64ae0781c",
+    "url": "/sadaic-client/static/js/2.a6092321.chunk.js"
+  },
+  {
+    "revision": "030fd1946d3513991185",
+    "url": "/sadaic-client/static/js/main.cc00d4fd.chunk.js"
+  },
+  {
+    "revision": "fb282b0d35c27489841f",
+    "url": "/sadaic-client/static/js/runtime-main.e11c17e4.js"
+  },
+  {
+    "revision": "11b179c43d2ca794f5423c840087ba4f",
+    "url": "/sadaic-client/static/media/logo.11b179c4.svg"
+  },
+  {
+    "revision": "bb68dcbcf7832e4ebca07493bdf765b4",
+    "url": "/sadaic-client/static/media/spotify.bb68dcbc.svg"
+  }
+]);
